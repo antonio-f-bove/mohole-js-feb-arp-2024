0f9657699d7a1f95a84d2854bb2b5926cc1a4228
anto <above@gualaclosures.com>
2024-02-08 10:16:16 +0100

    Display starships as cards

--- main.js
+++ main.js
@@ -23,12 +23,23 @@ async function main() {
 
     console.log(result, starships)
 
+    const list = document.getElementById('list')
+    starships.forEach(async starship => {
+        const newEl = document.createElement('li')
+        newEl.classList.add('col-6')
+        newEl.innerHTML = `
+            <div class="card mb-3">
+                <div class="card-body">
+                    <h5 class="card-title">${starship.name}</h5>
+                    <p class="card-text">${starship.model}</p>
+                    <button href="#" class="btn btn-primary">Films</button>
+                </div>
             </div>
-        </div>
 
     const list = document.getElementById('list')
     displayList.forEach(el => {
         list.append(el)
+        list.appendChild(newEl)
     });
 
 
