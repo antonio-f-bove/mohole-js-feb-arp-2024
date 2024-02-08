8481aead0d5d0343bde2dc3e2651d8b05ae21a7a
anto <above@gualaclosures.com>
2024-02-08 07:44:20 +0100

    Clean up

--- main.js
+++ main.js
@@ -1,30 +1,9 @@
-/*
-
-- fetch di tutte le starship da swapi
-- display nella lista
-    - name
-    - model
-    - films (questa è una lista)
-
-*/
 
 async function getStarships() {
     const response = await fetch('https://swapi.dev/api/starships')
     const data = await response.json()
     return data.results
 }
-
-function displayStarships(starships) {
-
-    // for (const s of starships) {
-    //     const newEl = document.createElement('li')
-    //     const name = document.createTextNode('name: ' + s.name)
-    //     const model = document.createTextNode(' | model: '+s.model)
-    //     newEl.appendChild(name)
-    //     newEl.appendChild(model)
-    //     list.appendChild(newEl)
-    // }
-
 
 }
 
@@ -44,18 +23,8 @@ async function main() {
 
     console.log(result, starships)
 
-    const displayList = starships.map(el => `
-    <div class="container">
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">${el.name}</h5>
-                <p class="card-text">${el.model}</p>
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
             </div>
         </div>
-    </div>
-    `)
 
     const list = document.getElementById('list')
     displayList.forEach(el => {
