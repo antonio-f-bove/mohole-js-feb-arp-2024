24a6875884c08e14f249212d5a07a30a7fc3d63e
anto <above@gualaclosures.com>
2024-02-12 13:38:52 +0100

    Refactor getStarships into button click, people stays broken

--- swapi/main.js
+++ swapi/main.js
@@ -1,6 +1,5 @@
-
-async function getStarships() {
-    const response = await fetch('https://swapi.dev/api/starships')
+async function getResourceList(resource) {
+    const response = await fetch(`https://swapi.dev/api/${resource}`)
     const data = await response.json()
     return data.results
 }
@@ -16,7 +15,31 @@ async function getFilm(filmUrl) {
 }
 
 async function main() {
-    const result = await getStarships()
+    const list = document.getElementById('list')
+    
+    // card templates (different for every resource)
+    const resources = ['people', 'starships']
+    const resourceButtons = resources.map(resource => document.getElementById(resource))
+    resourceButtons.forEach((el,_,array) => {
+        el.addEventListener('click', async (ev) => {
+            // remove and add .active
+            array.forEach(el => el.classList.remove('active'))
+            el.classList.add('active')
+
+            // remove page content
+            list.innerHTML = ''
+
+            switch (el.id) {
+                case 'starships': displayStarships(); break;
+                case 'people': displayPeople(); break;
+                default: displayFallback();
+            }
+        })
+    })
+}
+
+async function displayStarships() {
+    const result = await getResourceList('starships')
     /* Questa è la *guard clause*, la clausola di guardia che effettua un *early return*
      * nel caso in cui la lista risultasse vuota: tutto il codice della funzione sotto 
      * questa clausola non verrà eseguito.
@@ -25,16 +48,12 @@ async function main() {
         return 
     }
 
-    const starships = result.map(el => ({
-        name: el.name,
-        model: el.model,
-        films: el.films
-    }))
+    const starships = result.map(({name, model, films}) => ({name, model, films}))
 
-    console.log({result, starships})
+    console.log({starships})
 
-    const list = document.getElementById('list')
     starships.forEach(async starship => {
+        // TODO: extract function
         const newEl = document.createElement('li')
         newEl.classList.add('col-6')
         newEl.innerHTML = `
@@ -43,7 +62,7 @@ async function main() {
         <h5 class="card-title">${starship.name}</h5>
         <p class="card-text">${starship.model}</p>
         <div style="height: 80px;">
-            <button href="#" class="btn btn-primary">Films</button> <!--  class="overflow-y-auto" -->
+            <button class="btn btn-primary">Films</button> <!--  class="overflow-y-auto" -->
         </div>
     </div>
 </div>
@@ -54,7 +73,6 @@ async function main() {
                 const film = await getFilm(filmUrl)
                 filmsDetails.push(film)
             }
-            console.log({filmsDetails})
 
             const filmList = document.createElement('ul')
             filmList.classList.add('list-group', 'h-100', 'overflow-auto')
