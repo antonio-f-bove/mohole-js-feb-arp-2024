64f74d8866b0762646563a1b26a3867f03f8c535
anto <above@gualaclosures.com>
2024-02-20 10:31:40 +0100

    Js code for Promise.all

---
+++ swapi-test/main.js
@@ -0,0 +1,32 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+  main();
+});
+
+async function main() {
+  const resource = 'starships';
+
+  const starships = await fetchAll(resource);
+
+  console.log(starships);
+}
+
+
+async function fetchAll(resource) {
+  const res = await fetch(`https://swapi.dev/api/${resource}/`);
+  const json = await res.json();
+
+  const count = json.count;
+
+  const promises = [];
+  for (let i = 1; i <= count; i++) {
+    promises.push(fetch(`https://swapi.dev/api/${resource}/${i}/`));
+  }
+  const response = (await Promise.all(
+    promises
+      .map(p => p.then(r => r.json())))
+  ).filter(starship => starship.name);
+  // const starships = response.filter(s => s.name)
+
+  return response;
+}
