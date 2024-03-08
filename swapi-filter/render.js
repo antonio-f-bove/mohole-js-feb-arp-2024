76834cc5110e43fc57496b1b6a930c02ea2f6759
anto <above@gualaclosures.com>
2024-03-08 11:27:50 +0100

    Fixes character card

--- swapi-filter/render.js
+++ swapi-filter/render.js
@@ -9,13 +9,6 @@ export function renderList(htmlElements) {
 }
 
 export function renderCard(person) {
-  document.querySelectorAll('.list-group-item').forEach(p => {
-    if (p.classList.contains('active')) {
-      p.classList.remove('active')
-    }
-  });
-  person.html.classList.add('active');
-
   const card = document.createElement('div');
   card.className = 'card';
   card.innerHTML = `
@@ -24,12 +17,12 @@ export function renderCard(person) {
       <h6 class="card-subtitle mb-2 text-body-secondary">Character</h6>
       <p class="card-text">gender: ${person.gender}</p>
       <p class="card-text">birth year: ${person.birth_year}</p>
-      <p class="card-text"></p>
+      <!-- <p class="card-text"></p> -->
     </div>
   `;
 
-  // const main = document.querySelector('main');
-  // main.innerHTML = '';
-  // main.append = card;
+  const main = document.querySelector('main');
+  main.innerHTML = '';
+  main.append(card);
 }
 
