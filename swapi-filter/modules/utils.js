b7bfcf247fccf241693393820932d1a537c76e2d
anto <above@gualaclosures.com>
2024-03-14 15:50:00 +0100

    Add comment on film filter

--- swapi-filter/modules/utils.js
+++ swapi-filter/modules/utils.js
@@ -4,6 +4,12 @@ export function filterList(peopleWithHtml, nameFilter, filmFilter) {
     filteredList = filteredList.filter(el => el.name.toLowerCase().includes(nameFilter));
   }
   if (filmFilter) {
+    /*
+    el.films è un array di stringhe fatte in questo modo: "https://swapi.dev/api/films/1/"
+    con l'espressione `el.films.map(f => f[f.length - 2])` andiamo a prenderci l'id del film.
+    Vi ricordo che avevo dato per scontato che questi indici corrispondessero agli effettivi
+    Edisodi (1 -> Episodio I) ma non è così!!!
+    */
     filteredList = filteredList.filter(el => el.films.map(f => f[f.length - 2]).includes(filmFilter));
   }
 
