582e0b6d91515eb9527eaca2502d3ed0cf20cffc
anto <above@gualaclosures.com>
2024-03-14 18:29:52 +0100

    Add config form

--- fizzbuzz/main.js
+++ fizzbuzz/main.js
@@ -1,33 +1,35 @@
-const N = 25, fizz = 3, buzz = 5;
+function fizzbuzz() {
+  const N = 25, fizz = 3, buzz = 5;
 
-const grid = document.getElementById('grid');
+  const grid = document.getElementById('grid');
 
-for (let i = 1; i <= N; i++) {
-  const result = getFizzbuzz(i);
+  for (let i = 1; i <= N; i++) {
+    const result = getFizzbuzz(i);
 
-  const cell = document.createElement('div');
-  cell.textContent = result;
-  cell.className = 'cell';
-  if (typeof result == 'string') {
-    cell.classList.add(result);
+    const cell = document.createElement('div');
+    cell.textContent = result;
+    cell.className = 'cell';
+    if (typeof result == 'string') {
+      cell.classList.add(result);
+    }
+
+    grid.appendChild(cell);
   }
 
-  grid.appendChild(cell);
-}
 
+  function getFizzbuzz(i) {
+    let str = '';
 
-function getFizzbuzz(i) {
-  let str = '';
+    if (i % fizz === 0) {
+      str += 'fizz';
+    }
+    if (i % buzz === 0) {
+      str += 'buzz';
+    }
+    if (str === '') {
+      str = i;
+    }
 
-  if (i % fizz === 0) {
-    str += 'fizz';
+    return str;
   }
-  if (i % buzz === 0) {
-    str += 'buzz';
-  }
-  if (str === '') {
-    str = i;
-  }
-
-  return str;
 }
