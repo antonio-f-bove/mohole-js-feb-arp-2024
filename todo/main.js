4bf7f5a08ef0ff2ec43fe0765172c96947e150e0
above <antoniofbove@gmail.com>
2024-03-23 10:10:14 +0100

    Refactor html and select elements via js

---
+++ todo/main.js
@@ -0,0 +1,6 @@
+
+const filterBtns = document.querySelectorAll('header button');
+const todos = document.querySelector('main ul');
+const newTodo = document.querySelector('footer input');
+const addTodoBtn = document.querySelector('footer button');
+
