72c87111d340141bfa17d0f0348890e905bd0719
above <antoniofbove@gmail.com>
2024-03-23 18:48:51 +0100

    Add sample js todos

--- todo/main.js
+++ todo/main.js
@@ -1,6 +1,19 @@
+import { todos } from './data/todos.js';
 
 const filterBtns = document.querySelectorAll('header button');
-const todos = document.querySelector('main ul');
+const todoList = document.querySelector('main ul');
 const newTodo = document.querySelector('footer input');
 const addTodoBtn = document.querySelector('footer button');
 
+for (const todo of todos) {
+    const li = document.createElement('li');
+    li.className = "list-group-item d-flex justify-content-between";
+    li.innerHTML = `
+        <span></span>
+        <input type="checkbox" />
+    `;
+    li.querySelector('span').textContent = todo.description;
+    li.querySelector('input').checked = todo.done;
+
+    todoList.appendChild(li);
+}
