3aef3569f60b6afaec7de9ea09c6d9df0da397dc
above <antoniofbove@gmail.com>
2024-03-24 19:15:41 +0100

    Swap currentDate with todosLeft

--- todo/main.js
+++ todo/main.js
@@ -1,34 +1,40 @@
 import { todos } from './data/todos.js';
 
 const filterBtns = document.querySelectorAll('header button');
+const todosLeft = document.querySelector('header .badge');
 const todoList = document.querySelector('main ul');
 const addTodoInput = document.querySelector('footer input');
 const addTodoBtn = document.querySelector('footer button');
 
-for (const todo of todos) {
-    const li = document.createElement('li');
-    li.className = "list-group-item d-flex justify-content-between";
-    li.innerHTML = `
-        <span></span>
-        <input type="checkbox" />
-    `;
-    li.querySelector('span').textContent = todo.description;
-    li.querySelector('input').checked = todo.done;
-
-    todoList.appendChild(li);
-}
+todosLeft.textContent = todos.length;
+renderTodos();
 
 addTodoBtn.addEventListener('click', () => {
 
 });
 addTodoInput.addEventListener('keydown', (e) => {
-    if (e.key != 'Enter') {
-        return;
-    }
-    console.log('enter!')
-    todos.push({
-        description: addTodoInput.value,
-        done: false,
-    });
-    console.log(todos)
+  if (e.key != 'Enter') {
+    return;
+  }
+  console.log('enter!')
+  todos.push({
+    description: addTodoInput.value,
+    done: false,
+  });
+  console.log(todos)
 });
+
+function renderTodos() {
+  for (const todo of todos) {
+    const li = document.createElement('li');
+    li.className = "list-group-item d-flex justify-content-between";
+    li.innerHTML = `
+      <span></span>
+      <input type="checkbox" />
+      `;
+    li.querySelector('span').textContent = todo.description;
+    li.querySelector('input').checked = todo.done;
+
+    todoList.appendChild(li);
+  }
+}
