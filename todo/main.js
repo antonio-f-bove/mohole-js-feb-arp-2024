60d1751dafe24af7fe7e76de237622b49df706fe
above <antoniofbove@gmail.com>
2024-03-24 19:28:50 +0100

    Implement add todo

--- todo/main.js
+++ todo/main.js
@@ -6,25 +6,39 @@ const todoList = document.querySelector('main ul');
 const addTodoInput = document.querySelector('footer input');
 const addTodoBtn = document.querySelector('footer button');
 
-todosLeft.textContent = todos.length;
 renderTodos();
 
 addTodoBtn.addEventListener('click', () => {
-
+  if (!addTodoInput.value) {
+    return;
+  }
+  addTodo();
 });
 addTodoInput.addEventListener('keydown', (e) => {
   if (e.key != 'Enter') {
     return;
   }
-  console.log('enter!')
-  todos.push({
-    description: addTodoInput.value,
-    done: false,
-  });
-  console.log(todos)
+  addTodo();
 });
 
+function addTodo() {
+  const newTodo = {
+    description: addTodoInput.value,
+    done: false,
+  };
+  if (todos.map(({description}) => description).includes(newTodo.description)) {
+    // give feedback!
+    return;
+  }
+  todos.push(newTodo);
+  renderTodos();
+  addTodoInput.value = '';
+}
+
 function renderTodos() {
+  todosLeft.textContent = todos.length;
+  todoList.innerHTML = '';
+
   for (const todo of todos) {
     const li = document.createElement('li');
     li.className = "list-group-item d-flex justify-content-between";
